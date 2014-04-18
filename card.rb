c87e3da4e43686716a4996ffa8f35ecbac66a21c
chibimagic <git@chibimagic.org>
2014-04-18 16:14:53 -0700

    Initial commit: cards (admiral/president/kobol

---
+++ card.rb
@@ -0,0 +1,47 @@
+class Card
+  TITLE = ''
+  BODY = ''
+  
+  def to_s
+    title
+  end
+  
+  def title
+    self.class::TITLE
+  end
+  
+  def body
+    self.class::BODY
+  end
+end
+
+class AdmiralCard < Card
+  TITLE = 'Admiral'
+  BODY = <<eot
+When the fleet jumps, you draw 2 Destination Cards and choose 1.
+You control the nuke tokens.
+Action: Launch 1 nuke at a basestar (the nuke is removed from the game).
+Nukes damage a basestar twice on a roll of 1-2, and automatically destroy a basestar on a roll of 3-8.
+eot
+
+  ACTION_TEXT = "Launch 1 nuke at a basestar (the nuke is removed from the game)."
+end
+
+class PresidentCard < Card
+  TITLE = 'President'
+  BODY = <<eot
+At the start of the game, draw 1 Quorum Card. You control the hand of Quorum Cards.
+Action: Draw a Quorum Card into your hand.
+Quorum cards can be played as an action and specialize in increasing resources and dealing with unrevealed Cylons.
+eot
+  ACTION_TEXT = "Draw a Quorum Card into your hand."
+end
+
+class KobolObjectiveCard < Card
+  TITLE = 'Kobol'
+  BODY = <<eot
+Resolve when the following distance is traveled:
+4: Sleeper Agent phase.
+8: The next time the fleet jumps, the humans win the game (do not draw a destination card).
+eot
+end
